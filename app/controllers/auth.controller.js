c748e4687df30035138c18d51dcde27bed9de33d
Junaid Saleem <js.junaidsaleem@gmail.com>
2023-08-03 15:14:58 +0530

    NodeJS project for JWT User authentication

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -109,3 +109,12 @@ exports.signin = (req, res) => {
       });
     });
 };
+
+exports.signout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.status(200).send({ message: "You've been signed out!" });
+  } catch (err) {
+    this.next(err);
+  }
+};
