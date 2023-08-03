88da278cb79f5dc56d9969be69304d738154a09d
Junaid Saleem <js.junaidsaleem@gmail.com>
2023-08-03 16:32:23 +0530

    updated NodeJS project for JWT User authentication

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  URI: "mongodb+srv://junaid:AhJLltOzx83NgHGt@cluster0.cseadd9.mongodb.net",
   HOST: "0.0.0.0",
   PORT: 27017,
   DB: "test_db",
--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -113,7 +113,9 @@ exports.signin = (req, res) => {
 exports.signout = async (req, res) => {
   try {
     req.session = null;
-    return res.status(200).send({ message: "You've been signed out!" });
+    return res.status(200).send({
+      message: "You've been signed out!",
+    });
   } catch (err) {
     this.next(err);
   }
